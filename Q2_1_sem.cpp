6f55099741187f218038193d4b6e06fdd805c642
spjackso <54558673+spjackso@users.noreply.github.com>
2023-04-19 18:18:30 -0500

    Create Q2_1_sem.cpp

---
+++ Q2_1_sem.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <thread>
+#include <semaphore.h>
+using namespace std;
+sem_t sem;
+
+void runMeFirst() {
+    cout << "Receiving first thread..." << endl;
+    sem_wait(&sem);
+    cout << "Got it!" << endl;
+    this_thread::sleep_for(chrono::seconds(2));
+    cout << "Run me first" << endl;
+    sem_post(&sem);
+}
+
+void runMeSecond() {
+    cout << "Receiving second thread..." << endl;
+    sem_wait(&sem);
+    cout << "Got it!" << endl;
+    this_thread::sleep_for(chrono::seconds(2));
+    cout << "I am second" << endl;
+    sem_post(&sem);
+}
+
+int main() {
+   const int num_threads = 2;
+   sem_init(&sem, 0, num_threads);
+   thread threads[num_threads];
+   
+   for (int i = 0; i < num_threads; ++i) {
+       threads[i] = thread(runMeFirst, i);
+       threads[i] = thread(runMeSecond, i);
+}
+    
+    for (int i = 0; i < num_threads; ++i) {
+    threads[i].join();
+    }
+    
+    sem_destroy(&sem); // destroy the semaphore
+    return 0;
+}
